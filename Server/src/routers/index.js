d5a835338f4b38e4c95eef3f99cd828d452aeaff
EverChicas <everchicasp@gmail.com>
2020-03-22 00:08:25 -0600

    try-catch en login

--- Server/src/routers/index.js
+++ Server/src/routers/index.js
@@ -83,119 +83,124 @@ router.post('/registrar', (req, res) => {
 
 
 router.post('/login', (req, res) => {
-    let { user, password, extension, imagenBase64 } = req.body;
+    try {
+        let { user, password, extension, imagenBase64 } = req.body;
 
-    if (imagenBase64 && extension) {
+        if (imagenBase64 && extension) {
 
-        var allData = new AWS.DynamoDB.DocumentClient(aws_keys.dynamodb);
+            var allData = new AWS.DynamoDB.DocumentClient(aws_keys.dynamodb);
 
-        var params = {
-            TableName: "usuarios",
-            Limit: 100
-        };
+            var params = {
+                TableName: "usuarios",
+                Limit: 100
+            };
 
-        allData.scan(params, onScan);
+            allData.scan(params, onScan);
 
-        function onScan(err, data) {
-            if (err) {
-                console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
+            function onScan(err, data) {
+                if (err) {
+                    console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
 
-                let image = Buffer.from(imagenBase64, 'base64');
-                let filename = `${uuidv4()}.${extension}`;
+                    let image = Buffer.from(imagenBase64, 'base64');
+                    let filename = `${uuidv4()}.${extension}`;
 
-                //parametros para S3
-                let bucketname = 'bucketfotos-grupo11';
-                let folder = 'Temporal/';
-                let filepath = `${folder}${filename}`;
+                    //parametros para S3
+                    let bucketname = 'bucketfotos-grupo11';
+                    let folder = 'Temporal/';
+                    let filepath = `${folder}${filename}`;
 
-                var uploadParamsS3 = {
-                    Bucket: bucketname,
-                    Key: filepath,
-                    Body: image,
-                    ACL: 'public-read',
-                };
+                    var uploadParamsS3 = {
+                        Bucket: bucketname,
+                        Key: filepath,
+                        Body: image,
+                        ACL: 'public-read',
+                    };
 
-                S3.upload(uploadParamsS3, function async(err, dataImage) {
-                    if (err) {
-                        console.log('Error s3:', err);
-                        res.status(400).send({ "message": "No se pudo logiar" });
-                    } else {
-                        console.log('Upload success at:', dataImage.Location);
+                    S3.upload(uploadParamsS3, function async(err, dataImage) {
+                        if (err) {
+                            console.log('Error s3:', err);
+                            res.status(400).send({ "message": "No se pudo logiar" });
+                        } else {
+                            console.log('Upload success at:', dataImage.Location);
 
 
-                        data.Items.forEach(function (user) {
-                            var urlImagen = user.url;
-                            var nombreUser = user.user;
+                            data.Items.forEach(function (user) {
+                                var urlImagen = user.url;
+                                var nombreUser = user.user;
 
-                            console.log(urlImagen);
-                            console.log(filepath);
+                                console.log(urlImagen);
+                                console.log(filepath);
 
-                            //objeto rekognition
-                            var params = {
-                                SimilarityThreshold: 80,
-                                SourceImage: { /* required */
-                                    S3Object: {
-                                        Bucket: "bucketfotos-grupo11",
-                                        Name: filepath
+                                //objeto rekognition
+                                var params = {
+                                    SimilarityThreshold: 80,
+                                    SourceImage: { /* required */
+                                        S3Object: {
+                                            Bucket: "bucketfotos-grupo11",
+                                            Name: filepath
+                                        }
+                                    },
+                                    TargetImage: { /* required */
+                                        S3Object: {
+                                            Bucket: "bucketfotos-grupo11",
+                                            Name: urlImagen
+                                        }
                                     }
-                                },
-                                TargetImage: { /* required */
-                                    S3Object: {
-                                        Bucket: "bucketfotos-grupo11",
-                                        Name: urlImagen
-                                    }
-                                }
-                            };
+                                };
 
-                            rekognition.compareFaces(params, function (err, data) {
-                                if (err) {
-                                    console.log(err, err.stack);
-                                } else {
-                                    console.log(data);
-                                    res.status(200).send({ 'data': data });
-                                }
+                                rekognition.compareFaces(params, function (err, data) {
+                                    if (err) {
+                                        console.log(err, err.stack);
+                                    } else {
+                                        console.log(data);
+                                        res.status(200).send({ 'data': data });
+                                    }
+                                });
+
                             });
 
-                        });
-
-                    }
-                });
+                        }
+                    });
 
 
 
 
-            }
-        };
-    } else if (user && password) {
-        var allData = new AWS.DynamoDB.DocumentClient(aws_keys.dynamodb);
-
-        var params = {
-            TableName: "usuarios",
-            Limit: 100
-        };
-
-        allData.scan(params, onScan);
-
-        function onScan(err, data) {
-            if (err) {
-                console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                for (let i = 0; data.Count; i++) {
-                    var endb = data.Items[i];
-                    if (endb.user === user && endb.password === password) {
-                        console.log(endb);
-                        res.status(200).send({ 'user': `${endb.user}` });
-                        return;
-                    }
                 }
-                res.status(400).send({ 'message': 'Error de autenticacion' });
-            }
-        };
+            };
+        } else if (user && password) {
+            var allData = new AWS.DynamoDB.DocumentClient(aws_keys.dynamodb);
 
-    } else {
+            var params = {
+                TableName: "usuarios",
+                Limit: 100
+            };
+
+            allData.scan(params, onScan);
+
+            function onScan(err, data) {
+                if (err) {
+                    console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    for (let i = 0; data.Count; i++) {
+                        var endb = data.Items[i];
+                        if (endb.user === user && endb.password === password) {
+                            console.log(endb);
+                            res.status(200).send({ 'user': `${endb.user}` });
+                            return;
+                        }
+                    }
+                    res.status(400).send({ 'message': 'Error de autenticacion' });
+                }
+            };
+
+        } else {
+            res.status(400).send({ 'message': 'Error de autenticacion' });
+        }
+    } catch (error) {
         res.status(400).send({ 'message': 'Error de autenticacion' });
     }
+
 });
 
 module.exports = router;
