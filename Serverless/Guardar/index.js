bdf05b94aa8483d8e24309d252b8300993d57836
Alejandro <61027811+alerod620@users.noreply.github.com>
2020-03-22 22:01:20 -0600

    Update index.js

--- Serverless/Guardar/index.js
+++ Serverless/Guardar/index.js
@@ -13,7 +13,6 @@ exports.handler = (event, context, callback) => {
     const imagenBase64 = body.imagenBase64;
     const uuid = Date.now().toString();
 
-    var salida = "";
     if (imagenBase64 && extension) {
         let image = Buffer.from(imagenBase64, 'base64');
         let filename = `${user}-${uuid}.${extension}`;
@@ -35,6 +34,55 @@ exports.handler = (event, context, callback) => {
             if (err) {
                 callback(err, { 'message': err });
             } else {
+                
+                /*
+                var params_label = {
+                  Image: {
+                    S3Object: {
+                      Bucket: 'bucketfotos-grupo11',
+                      Name: filepath
+                    },
+                  },
+                  MaxLabels: 1,
+                  MinConfidence: 80
+                };
+                
+                var etiqueta = "";
+                
+                rekognition.detectLabels(params_label, function(err, data){
+                    if(err)
+                    {
+                        callback(err, null);
+                    }
+                    else
+                    {
+                        data.Labels.forEach(label => {
+                            etiqueta = `${label.Name}`;
+                        });
+                    }
+                });
+                
+                //GUARDAR EN LA BASE DE DATOS
+                var params_album = {
+                    Item: {
+                        "Album": etiqueta,
+                        "Foto": data.Location
+                    },
+                    TableName: "Album"
+                };
+
+                ddb.put(params_album, function (err, dataddb) {
+                    if (err) {
+                        console.log("error en en guardar nueva foto");
+                        callback(err, { 'message': err });
+                    } else {
+                        console.log('Save success nueva foto');
+
+                        
+                    }   
+                });
+                */
+                
                 //GUARDAR EN LA BASE DE DATOS
                 var paramsdb = {
                     Item: {
@@ -51,85 +99,87 @@ exports.handler = (event, context, callback) => {
                     } else {
                         console.log('Save success nueva foto');
 
-                        //--------------------------
-                        //Parametros para jalar la imagen de perfil
-                        //----------------------------
-                        var params_con = {
-                            TableName: "usuarios",
-                            Limit: 100
-                        };
-
-                        ddb2.scan(params_con, onScan);
-
-                        function onScan(err, data2) {
-                            if (err) {
-                                console.log("error en buscar usuario");
-                                console.log(err);
-                            } else {
-                                for (let i = 0; i < data2.Count; i++) {
-                                    var endb = data2.Items[i];
-                                    if (endb.user === user) {
-                                        console.log('Se encontro usuario');
-                                        console.log(endb.url);
-                                        console.log(filepath);
-                                        //REKOGNITION
-                                        var params_reko = {
-                                            SimilarityThreshold: 80,
-                                            SourceImage: {
-                                                S3Object: {
-                                                    Bucket: "bucketfotos-grupo11",
-                                                    Name: endb.url
-                                                }
-                                            },
-                                            TargetImage: {
-                                                S3Object: {
-                                                    Bucket: "bucketfotos-grupo11",
-                                                    Name: filepath
-                                                }
-                                            }
-                                        };
-
-                                        rekognition.compareFaces(params_reko, function (err, datareko) {
-                                            if (err) {
-                                                console.log("error rekognition");
-                                                callback(err, null);
-                                            } else {
-                                                try {
-                                                    if (datareko.FaceMatches[0].Similarity >= 80) {
-                                                        console.log("Si son parecidos");
-                                                        //INSERTAR A TABLA APARECE
-                                                        var params_ap = {
-                                                            Item: {
-                                                                "Usuario": { S: user },
-                                                                "Foto": { S: data.Location }
-                                                            },
-                                                            TableName: "Aparece"
-                                                        };
-                                                        console.log(user)
-                                                        console.log(data.Location)
-                                                        ddb3.putItem(params_ap, function (err, data3) {
-                                                            if (err) {
-                                                                console.log("error en aparece");
-                                                                callback(err, null);
-                                                            } else {
-                                                                console.log("guarde en aparece");
-                                                                callback(null, { 'message': 'ddb success' });
-                                                            }
-                                                        });
-                                                    }
-                                                } catch (e) {
-                                                    callback(err, null);
-                                                }
-                                            }
-                                        });
-
-                                    }
-                                }
-                                console.log("error no se encontro usuario");
-                            }
-                        };
-                    }
+                        
+                    }   
                 });
+                
+                //--------------------------
+                //Parametros para jalar la imagen de perfil
+                //----------------------------
+                var params_con = {
+                    TableName: "usuarios",
+                    Limit: 100
+                };
+
+                ddb2.scan(params_con, onScan);
+
+                function onScan(err, data2) {
+                    if (err) {
+                        console.log("error en buscar usuario");
+                        console.log(err);
+                    } else {
+                        for (let i = 0; i < data2.Count; i++) {
+                            var endb = data2.Items[i];
+                            if (endb.user === user) {
+                                console.log('Se encontro usuario');
+                                console.log(endb.url);
+                                console.log(filepath);
+                                //REKOGNITION
+                                var params_reko = {
+                                    SimilarityThreshold: 80,
+                                    SourceImage: {
+                                        S3Object: {
+                                            Bucket: "bucketfotos-grupo11",
+                                            Name: endb.url
+                                        }
+                                    },
+                                    TargetImage: {
+                                        S3Object: {
+                                            Bucket: "bucketfotos-grupo11",
+                                            Name: filepath
+                                        }
+                                    }
+                                };
+
+                                rekognition.compareFaces(params_reko, function (err, datareko) {
+                                    if (err) {
+                                        console.log("error rekognition");
+                                        callback(err, null);
+                                    } else {
+                                        try {
+                                            if (datareko.FaceMatches[0].Similarity >= 80) {
+                                                console.log("Si son parecidos");
+                                                //INSERTAR A TABLA APARECE
+                                                var params_ap = {
+                                                    Item: {
+                                                        "Usuario": { S: user },
+                                                        "Foto": { S: data.Location }
+                                                    },
+                                                    TableName: "Aparece"
+                                                };
+                                                console.log(user)
+                                                console.log(data.Location)
+                                                ddb3.putItem(params_ap, function (err, data3) {
+                                                    if (err) {
+                                                        console.log("error en aparece");
+                                                        callback(err, null);
+                                                    } else {
+                                                        console.log("guarde en aparece");
+                                                        callback(null, { 'message': 'ddb success' });
+                                                    }
+                                                });
+                                            }
+                                        } catch (e) {
+                                            callback(err, null);
+                                        }
+                                    }
+                                });
+
+                            }
+                        }
+                        console.log("error no se encontro usuario");
+                    }
+                };
             }
         });
     }
